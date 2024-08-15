f8f25a551bd7ef2cd4ff1f01facaf56f2663206b
Larissa Czarnik <104375004+LARIGAMER14@users.noreply.github.com>
2024-08-15 09:55:57 -0300

    t

--- aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["", "", "", "", "", "", ""];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
